e3234d19acffa80b16df0bbd30d54efe188396a9
Victor Koome <koomev786@gmail.com>
2024-03-13 15:10:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="content">
+        <div>
+          <h2>Welcome to Infinite Moments Studios</h2>
+          <p>
+            where every moment is a masterpiece. Step into a world of timeless
+            elegance and captivating beauty, where our lenses capture the
+            essence of life's most precious moments.{" "}
+          </p>
+          <button>View Gallery</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
